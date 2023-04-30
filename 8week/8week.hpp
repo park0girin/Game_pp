2a86a50efe54a63015c885ef7554ac878978711c
park0girin <park0girin@yuhan.ac.kr>
2023-04-30 23:17:33 +0900

    ESC구현

---
+++ 8week/8week.hpp
@@ -0,0 +1,127 @@
+#pragma once // #define _IS_THIS_HEADER_INCLUDED
+
+#define Key_ESC 27
+#define Key_RIGHT 'd'
+#define Key_LEFT 'a'
+#define Key_ENTER 13
+
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BLUE     "\x1b[34m"
+#define ANSI_COLOR_MAGENTA  "\x1b[35m"
+#define ANSI_COLOR_CYAN     "\x1b[36m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
+int esc = 0;
+namespace NameSpace
+{
+	void gotoxy(int x, int y) {
+		//x, y 좌표 설정
+		COORD pos = { x,y };
+		//커서 이동
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	}
+
+	class GameLoop
+	{
+	public:
+
+		bool isGameRunning = false;
+
+		GameLoop() {}
+		~GameLoop() {}
+
+		void Initialize()
+		{
+			isGameRunning = true;
+			gotoxy(0, 10);
+			std::cout << ANSI_COLOR_MAGENTA "초기화 완료" << std::endl;
+		}
+
+		void Release()
+		{
+			gotoxy(0, 10);
+			std::cout << ANSI_COLOR_MAGENTA "시스템 해제 완료" << std::endl;
+		}
+
+		void Update()
+		{
+			gotoxy(0, 10);
+			std::cout << ANSI_COLOR_MAGENTA "업데이트중 완료" << std::endl;
+			if (_kbhit())
+			{
+
+				keyEvent(_getch());
+			}
+
+		}
+		void Render()
+		{
+			gotoxy(0, 10);
+			std::cout << ANSI_COLOR_MAGENTA "렌더링 완료" << std::endl;
+		}
+
+		void Run()
+		{
+			Initialize();
+
+			while (isGameRunning)
+			{
+				Update();
+				Render();
+			}
+
+			Release();
+
+		}
+
+		void keyEvent(char KeyInput)
+		{
+			switch (KeyInput)
+			{
+			case Key_LEFT:	//왼쪽 화살표키
+				gotoxy(2, 5);
+				std::cout << ANSI_COLOR_YELLOW"왼쪽이 눌렸다!  " << std::endl;
+				esc = 0;
+				break;
+			case Key_RIGHT:	//오른쪽 화살표키
+				gotoxy(2, 5);
+				std::cout << ANSI_COLOR_YELLOW"오른쪽이 눌렸다!" << std::endl;
+				esc = 0;
+				break;
+				/* 27 : esc 고 게임 종료키 입니다.
+				 *	한번 눌렀을땐 일시정지
+				 *  두번 눌렀을땐 게임 종료
+				 */
+			case Key_ESC:
+				esc++;
+				if (esc == 1)
+				{
+					gotoxy(2, 5);
+					std::cout << ANSI_COLOR_CYAN"esc가 눌렸다!   " << std::endl;
+					break;
+				}
+				if (esc == 2)
+				{
+					gotoxy(2, 5);
+					std::cout << ANSI_COLOR_CYAN"게임이 종료되었다!" << std::endl;
+					esc = 0;
+					isGameRunning = false;
+				}
+				//gotoxy(5, 6);
+				//std::cout << ANSI_COLOR_GREEN"esc=" + esc << std::endl;
+				break;
+			case Key_ENTER:
+				gotoxy(2, 5);
+				std::cout << ANSI_COLOR_YELLOW"엔터가 눌렸다!  " << std::endl;
+				esc = 0;
+				break;
+			default:
+				gotoxy(2, 5);
+				esc = 0;
+				break;
+			}
+		}
+	};
+}
